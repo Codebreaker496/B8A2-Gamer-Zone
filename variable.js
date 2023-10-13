69c77147866dd13dc7a297f4412cd6c5cea709a3
codebreaker496 <mamun.abdulla@gmail.com>
2023-10-13 17:56:55 +0600

    play section

---
+++ variable.js
@@ -0,0 +1,44 @@
+// Numeric
+
+var price = 100;
+var age = 35;
+var temperature = 38;
+
+
+// ek ba ekadhik word thakle "string"
+
+var name = mamun;
+var name = "Abdullah Al mamun" ;
+var location = "Belkuchi Sirajgonj";
+var name = 'Limon'; 
+// You can use both single or double quote.
+
+
+// Boolean data type : '0 or 1' where it shows as "true or false"
+
+var single = true;
+var married = false;
+
+// true & false here are keywords hence no need for quotation.
+
+// variable-name rules :
+
+//1. variable name cannot be any keywords
+
+var return = 29;
+var true = flase;
+
+// 2. variable name has to be in one word no space.
+
+var ami limon = true;
+
+// 3. Variable name cannot have quotation.
+
+var "Limon" = true;
+
+// 4. variable name cannot start with a number but can end with one.
+
+var 99club = 1999;
+var club23 = 2023;
+
+// 5. 
